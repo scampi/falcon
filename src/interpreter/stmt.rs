d055b2c4635fbad8a8789d1212229cacf4f26bf4
Stéphane Campinas <stephane.campinas@gmail.com>
2019-01-08 21:12:40 +0100

    support break statement

--- src/interpreter/stmt.rs
+++ src/interpreter/stmt.rs
@@ -1,38 +1,61 @@
 use crate::{
     errors::EvaluationError,
-    interpreter::{Context, Eval},
+    interpreter::{value::Value, Context, Eval},
     parser::ast::Stmt,
 };
 
+#[derive(Debug)]
+pub enum StmtResult {
+    Break,
+    Continue,
+    Return(Value),
+    Exit(isize),
+}
+
 impl Eval for Stmt {
-    type EvalResult = ();
-    fn eval(&self, cxt: &mut Context) -> Result<(), EvaluationError> {
+    type EvalResult = Option<StmtResult>;
+    fn eval(&self, cxt: &mut Context) -> Result<Option<StmtResult>, EvaluationError> {
         match self {
             Stmt::For(init, cond, step, body) => match (init, cond, step) {
                 (Some(init), Some(cond), Some(step)) => {
                     init.eval(cxt)?;
                     while cond.eval(cxt)?.as_bool() {
-                        body.eval(cxt)?;
+                        if let Some(res) = body.eval(cxt)? {
+                            match res {
+                                StmtResult::Break => break,
+                                _ => unimplemented!("{:?}", res),
+                            }
+                        }
                         step.eval(cxt)?;
                     }
-                    Ok(())
+                    Ok(None)
                 },
                 (..) => unimplemented!(),
             },
             Stmt::DoWhile(cond, stmt) => {
                 loop {
-                    stmt.eval(cxt)?;
+                    if let Some(res) = stmt.eval(cxt)? {
+                        match res {
+                            StmtResult::Break => break,
+                            _ => unimplemented!("{:?}", res),
+                        }
+                    }
                     if !cond.eval(cxt)?.as_bool() {
                         break;
                     }
                 }
-                Ok(())
+                Ok(None)
             },
             Stmt::While(cond, stmt) => {
                 while cond.eval(cxt)?.as_bool() {
-                    stmt.eval(cxt)?
+                    if let Some(res) = stmt.eval(cxt)? {
+                        match res {
+                            StmtResult::Break => break,
+                            _ => unimplemented!("{:?}", res),
+                        }
+                    }
                 }
-                Ok(())
+                Ok(None)
             },
             Stmt::IfElse(cond, ok, ko) => {
                 if cond.eval(cxt)?.as_bool() {
@@ -40,16 +63,22 @@ impl Eval for Stmt {
                 } else if let Some(ko) = ko {
                     ko.eval(cxt)
                 } else {
-                    Ok(())
+                    Ok(None)
                 }
             },
             Stmt::Block(stmts) => {
                 for stmt in &stmts.0 {
-                    stmt.eval(cxt)?;
+                    if let Some(res) = stmt.eval(cxt)? {
+                        match res {
+                            StmtResult::Break => return Ok(Some(StmtResult::Break)),
+                            _ => unimplemented!("{:?}", res),
+                        }
+                    }
                 }
-                Ok(())
+                Ok(None)
             },
-            Stmt::Expr(e) => e.eval(cxt).map(|_| ()),
+            Stmt::Expr(e) => e.eval(cxt).map(|_| None),
+            Stmt::Break => Ok(Some(StmtResult::Break)),
             _ => unimplemented!("{:?}", self),
         }
     }
@@ -120,6 +149,28 @@ mod tests {
         assert_eq!(
             cxt.vars.get("a"),
             Value::from("01234".to_owned()),
+            "{:?}",
+            stmt
+        );
+    }
+
+    #[test]
+    fn r#break() {
+        let mut cxt = Context::new();
+        let stmt = get_stmt("while (a < 10) if (a < 5) a += 2; else break;");
+        stmt.eval(&mut cxt).unwrap();
+        assert_eq!(cxt.vars.get("a"), Value::from(6.0), "{:?}", stmt);
+
+        let stmt = get_stmt("do if (b < 5) b += 2; else break; while (b < 10)");
+        stmt.eval(&mut cxt).unwrap();
+        assert_eq!(cxt.vars.get("b"), Value::from(6.0), "{:?}", stmt);
+
+        let stmt =
+            get_stmt(r#"for (i = 0; i < 10; i++) { c = c i; if (c == "2100123") break; c = i c }"#);
+        stmt.eval(&mut cxt).unwrap();
+        assert_eq!(
+            cxt.vars.get("c"),
+            Value::from("2100123".to_owned()),
             "{:?}",
             stmt
         );
